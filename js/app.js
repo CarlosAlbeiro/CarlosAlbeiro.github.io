f5924be1c64e3d88ff0e55fc7d68e4371f85b308
CarlosAlbeiro <carlosalbeirogaleano@hotmail.com>
2024-03-08 21:26:20 -0500

    implementacion de perfil

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
-(function () {
-    window.addEventListener('scroll', () => {
-        scrollActual = window.scrollY;
-        if (scrollActual > 801.1636352539062) {
-            $("#menu").css("display", "block")
-            $("#menu").show();
-        } else {
-            $("#menu").hide("slow");
-        }
-    });
-})();
+function esMovil() {
+  return /Mobi|Android/i.test(navigator.userAgent);
+}
 
+(function () {
+  window.addEventListener("scroll", () => {
+    scrollActual = window.scrollY;
+    console.log("====================================");
+    console.log(scrollActual);
+    console.log("====================================");
+    let medida=0;
+    if (esMovil()) {
+      medida=582.9818115234375;
+    } else {
+      medida=636.2181396484375;
+    }
+    if (scrollActual > medida) {
+      $("#menu").css("display", "block");
+      $("#menu").show();
+    } else {
+      $("#menu").hide("slow");
+    }
+  });
+})();
